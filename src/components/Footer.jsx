99161e1fe2b12f7a7a79010ed6edcbfea66ceb4b
ianca <S22310400@student.unklab.ac.id>
2025-11-04 11:57:46 +0800

    all Components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="bg-gray-200 text-gray-600 p-4 text-center">
+      © 2025 Pokédex Project — made with ❤ by our team
+    </footer>
+  );
+}
+
+export default Footer;
