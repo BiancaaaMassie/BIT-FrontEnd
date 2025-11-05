f1e6b19c995b0b0197722a3baa867b09998527e8
Injills223 <S22310335@student.unklab.ac.id>
2025-11-06 00:03:36 +0800

    push generations

---
+++ src/components/Header/Generations/Generations.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Generations() {
+    const generations = ["Gen 1", "Gen 2", "Gen 3", "Gen 4"];
+  return (
+    <div>
+      <p>Daftar Gen 1 hingga Gen 4.</p>
+    </div>
+  );
+}
+
+export default Generations;
