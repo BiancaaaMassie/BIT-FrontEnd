97c30b74a2cf1fe086ab359959e405effd0da991
Injills223 <S22310335@student.unklab.ac.id>
2025-11-10 02:40:49 +0800

    conflic merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import React from "react";
-import Home from "./components/Home/Home.jsx";
 
+import React from "react";
+import Header from "./components/Header/Header.jsx";
+import Home from "./components/Home/Home.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 
 export default function App() {
   return (
     <div className="min-h-screen flex flex-col bg-gray-50">
+      <Header />
       <main className="flex-grow">
         <Home />
       </main>
+      <Footer />
     </div>
   );
 }
