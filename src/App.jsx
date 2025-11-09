0dca6fe36aaf93622a20ba3909c7e00163acee4d
Injills223 <S22310335@student.unklab.ac.id>
2025-11-10 01:01:48 +0800

    update logo2

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import Header from "./components/Header";
-import Home from "./components/Home";
-import Footer from "./components/Footer";
+// src/App.jsx
+import React from "react";
+import Header from "./components/Header/Header.jsx";
+import Home from "./components/Home/Home.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 
-function App() {
+export default function App() {
   return (
-    <div className="min-h-screen flex flex-col bg-gray-100 text-center">
-      <Header title="Pokédex Project" />
-      <main className="flex-1 p-6">
+    <div className="min-h-screen flex flex-col bg-gray-50">
+      <Header />
+      <main className="flex-grow">
         <Home />
       </main>
       <Footer />
     </div>
   );
 }
-
-export default App;
